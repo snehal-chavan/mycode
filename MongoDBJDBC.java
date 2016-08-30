020a69928b86c8fe4a9425431896ee83fee7dfe2
snehal chavan <chavansnehal247@gmail.com>
2016-08-31 03:55:57 +0530

    first code

---
+++ MongoDBJDBC.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mongodbjdbc;
+
+/**
+ *
+ * @author snehal
+ */
+import com.mongodb.MongoClient;
+import com.mongodb.MongoException;
+import com.mongodb.WriteConcern;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import com.mongodb.DBCursor;
+
+import com.mongodb.ServerAddress;
+import java.util.Arrays;
+
+public class MongoDBJDBC {
+
+   public static void main( String args[] ) {
+	
+      try{
+		
+         // To connect to mongodb server
+         MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
+			
+         // Now connect to your databases
+         DB db = mongoClient.getDB( "dontknow_db" );
+         System.out.println("Connect to database successfully");
+         
+	 DBCollection coll = db.getCollection("mycol");
+         System.out.println("Collection created successfully");	
+   BasicDBObject doc = new BasicDBObject("title", "MongoDB").
+            append("description", "database").
+            append("likes", 100).
+            append("url", "http://www.tutorialspoint.com/mongodb/").
+            append("by", "tutorials point");
+				
+         coll.insert(doc);
+         System.out.println("Document inserted successfully");
+         
+            DBCursor cursor = coll.find();
+         int i = 1;
+			
+         while (cursor.hasNext()) { 
+            System.out.println("Inserted Document: "+i); 
+            System.out.println(cursor.next()); 
+            i++;
+         }
+      }catch(Exception e){
+         System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+      }
+   }
+}
+
